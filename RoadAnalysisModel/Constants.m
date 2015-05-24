b5ecbdcd9930bd102357e59b1d7300cd1661e24f
Noam <mizrachinoam@gmail.com>
2015-05-24 11:54:50 +0300

    change run mode default to random

--- RoadAnalysisModel/Constants.m
+++ RoadAnalysisModel/Constants.m
@@ -2,16 +2,16 @@ classdef Constants
     properties (Constant = true)
         EDGE = 1000;
         L=200;
-        NUM_OF_STEPS = 1;
+        NUM_OF_STEPS = 2;
         ROAD_WIDTH = 200;
         ROAD_DISTANCE = 1000;
         ABOVE_ROAD_HEIGHT = 200;
         MIN_HEIGHT_ABOVE_ROAD = 2;
         ANGLE_OF_VIEW = 50;
         CAMERA_HEIGHT = 130;
-        CAMERA_ROTATION_IN_DEGREES = 50;
+        CAMERA_ROTATION_IN_DEGREES = 0;
         STEP_SIZE = 50;
-        NUM_OF_POINT_TO_GENERATE = 10;
+        NUM_OF_POINT_TO_GENERATE = 100;
         NUM_OF_CAMERA_HISTORY = 2;
         DISPARITY_THRESHOLD=5;
 
@@ -29,12 +29,12 @@ classdef Constants
 		
         %draw
         drawRoad = true;
-        drawFOVarea = true;
+        drawFOVarea = false;
         drawFOVplane = false;
         drawCameraPlane = true;
         drawCameraRoadIntersectionPoints = false;
         drawClassification = false;
-        drawEpipole = false;
+        drawEpipole = true;
         drawEpipolarLines = false;
         drawDisparity = false;
         drawDisparityClassification = false;
