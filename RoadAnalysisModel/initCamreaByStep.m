e77af4ea9333151d8025bf6b8d6dea60ac1daab6
Noam <mizrachinoam@gmail.com>
2015-08-11 00:06:55 +0300

    add camera rotation mistake horizon and yaw

--- RoadAnalysisModel/initCamreaByStep.m
+++ RoadAnalysisModel/initCamreaByStep.m
@@ -1,12 +1,12 @@
 function [R, Ct] = initCamreaByStep(step)
-    cameraRotationNoise = floor(Constants.MIN_CAMERA_ROTATION_IN_DEGREES_MISTAKE + (Constants.MAX_CAMERA_ROTATION_IN_DEGREES_MISTAKE-Constants.MIN_CAMERA_ROTATION_IN_DEGREES_MISTAKE+1)*rand(1));
+    cameraRotationNoise = floor(Constants.MIN_CAMERA_ROTATION_HORIZON_IN_DEGREES_MISTAKE + (Constants.MAX_CAMERA_ROTATION_HORIZON_IN_DEGREES_MISTAKE-Constants.MIN_CAMERA_ROTATION_HORIZON_IN_DEGREES_MISTAKE+1)*rand(1));
     cameraRotationHorizonWithNoise = Constants.CAMERA_ROTATION_HORIZON_IN_DEGREES + cameraRotationNoise;
     horizon = degtorad(cameraRotationHorizonWithNoise);
     Rx = [1, 0, 0 ; ...
          0, cos(horizon), sin(horizon) ; ...
          0, -sin(horizon), cos(horizon)];
 
-     cameraRotationNoise = floor(Constants.MIN_CAMERA_ROTATION_IN_DEGREES_MISTAKE + (Constants.MAX_CAMERA_ROTATION_IN_DEGREES_MISTAKE-Constants.MIN_CAMERA_ROTATION_IN_DEGREES_MISTAKE+1)*rand(1));
+     cameraRotationNoise = floor(Constants.MIN_CAMERA_ROTATION_YAW_IN_DEGREES_MISTAKE + (Constants.MAX_CAMERA_ROTATION_YAW_IN_DEGREES_MISTAKE-Constants.MIN_CAMERA_ROTATION_YAW_IN_DEGREES_MISTAKE+1)*rand(1));
      cameraRotationYawWithNoise = Constants.CAMERA_ROTATION_YAW_IN_DEGREES + cameraRotationNoise;
      yaw = degtorad(cameraRotationYawWithNoise);
      Ry = [cos(yaw),  0, sin(yaw) ; ...
