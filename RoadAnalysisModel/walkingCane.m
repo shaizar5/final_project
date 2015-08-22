80f1eff4055f45424bd8df563bc96464b33e857c
shaizar <shaizar@gmail.com>
2015-08-22 16:47:54 +0300

    update walking cane

--- RoadAnalysisModel/walkingCane.m
+++ RoadAnalysisModel/walkingCane.m
@@ -7,8 +7,13 @@ function walkingCane(ct,step)
      walkingCaneHand = -1;
   end
   
+  changeDir=1;
+  if (step>=5 && step<10)
+      changeDir = -1;
+  end
+  newStep = mod(step, 5);
   caneStartPos = ct+[walkingCaneHand*Constants.CANE_DISTANCE_FROM_BODY_CENTER,0,0]';
-  caneEndPos = [Constants.CANE_MOVEMENT*step,0,Constants.CANE_END_POINT_DISTANCE*step]';
+  caneEndPos = [Constants.CANE_MOVEMENT*newStep*changeDir,0,Constants.CANE_END_POINT_DISTANCE*step]';
   caneRadius = Constants.CANE_RADIUS;
   caneDensity = Constants.CANE_DENSITY;
   [Cylinder, EndPlate1, EndPlate2] = Cylinder3D(caneStartPos,caneEndPos,caneRadius,caneDensity,'k',1,0);
